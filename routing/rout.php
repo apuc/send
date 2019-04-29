6fb7b434ab9b65d3c68dd99c02cfb4df2fc9716c
Kavalar <apuc06@mail.ru>
2019-04-29 23:25:04 +0300

    first

--- routing/rout.php
+++ routing/rout.php
@@ -5,6 +5,7 @@ use Phroute\Phroute\Dispatcher;
 $collector = new RouteCollector();
 $collector->get('/', ['workspace\controllers\MainController', 'actionIndex']);
 $collector->get('/send-form', ['workspace\controllers\SendController', 'actionIndex']);
+$collector->get('/send', ['workspace\controllers\SendController', 'actionSend']);
 $collector->get('products', function(){
     return 'Create Product';
 });
--- workspace/controllers/SendController.php
+++ workspace/controllers/SendController.php
@@ -10,13 +10,48 @@ namespace workspace\controllers;
 
 
 use core\Controller;
+use core\Debug;
+use Swift_Mailer;
+use Swift_Message;
+use Swift_SmtpTransport;
 
 class SendController extends Controller
 {
 
     public function actionIndex()
     {
+        return $this->render('send/index.tpl');
+    }
 
+    public function actionSend()
+    {
+        $json = file_get_contents(ROOT_DIR . '/json/user.json');
+        $arr = json_decode($json, true);
+//        foreach ($arr as $item) {
+//            Debug::prn($item);
+//        }
+        $res = $this->send([]);
+        Debug::prn($res);
+        return 'send';
+    }
+
+    private function send($user)
+    {
+        $transport = (new Swift_SmtpTransport('mail.adm.tools', 25))
+            ->setUsername('info@behance.space')
+            ->setPassword('123edsaqw');
+
+        $mailer = new Swift_Mailer($transport);
+
+        $message = (new Swift_Message('Новый адрес'))
+            ->setFrom(['info@behance.space' => 'Info'])
+            ->setTo(['apuc06@mail.ru'])
+            ->setBody('Наш новый адрес')
+            ->addPart($this->getTpl('send/msg.tpl'), 'text/html');
+
+        $result = $mailer->send($message);
+
+        return $result;
     }
 
 }
